12c609d78abe247becf3ecee1cc6fbbb293bed15
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2019-10-10 12:19:05 +0200

    Prepare publish NPM

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Act } from './Act';
+export { ActHistory } from './ActHistory';
+export { ActType } from './ActType';
+export { Message } from './Message';
+export { Signification } from './Signification';
+export { User } from './User';
